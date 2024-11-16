67bf8214f46112c3cc8a0442590f43af15a1cf58
Kreato <kreato@kreato.dev>
2024-11-16 03:07:30 +0300

    akiri/system: use Touch ID, even on screen share/recording

--- hosts/akiri/system.nix
+++ hosts/akiri/system.nix
@@ -1,5 +1,10 @@
 {
   security.pam.enableSudoTouchIdAuth = true;
+  system.defaults.CustomUserPreferences = {
+  		"com.apple.security.authorization" = {
+  			"ignoreArd" = true;
+		};
+	};
   system.stateVersion = 5;
   nixpkgs.hostPlatform = "aarch64-darwin";
 }
