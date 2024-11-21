28f1c87e8625b6541aec1835694912ef7b31e2a4
Kreato <kreato@kreato.dev>
2024-11-21 06:44:08 +0300

    add cleanshot widget to sketchybar

---
+++ hosts/akiri/userConfigurations/sketchybar/items/widgets/cleanshot.lua
@@ -0,0 +1,25 @@
+local icons = require("icons")
+local colors = require("colors")
+local settings = require("settings")
+
+local popup_width = 250
+
+local cleanshot = sbar.add("item", {
+  position = "right",
+  label = { drawing = false },
+  background = { color = colors.bg1 },
+  popup = { align = "center", height = 30 },
+  icon = { 
+	string = icons.screenshare,
+	padding_right = 3,
+  },
+  width = 35,
+  align = "center",
+})
+
+
+local function toggle_details()
+	sbar.exec("osascript -e 'tell application \"System Events\" to tell process \"CleanShot X\" to perform action \"AXPress\" of menu bar item 1 of menu bar 2'")
+end
+
+cleanshot:subscribe("mouse.clicked", toggle_details)
--- hosts/akiri/userConfigurations/sketchybar/items/widgets/wifi.lua
+++ hosts/akiri/userConfigurations/sketchybar/items/widgets/wifi.lua
@@ -8,53 +8,6 @@ sbar.exec("killall network_load >/dev/null; $CONFIG_DIR/helpers/event_providers/
 
 local popup_width = 250
 
-local wifi_up = sbar.add("item", "widgets.wifi1", {
-  position = "right",
-  padding_left = -5,
-  width = 0,
-  icon = {
-    padding_right = 0,
-    font = {
-      style = settings.font.style_map["Bold"],
-      size = 9.0,
-    },
-    string = icons.wifi.upload,
-  },
-  label = {
-    font = {
-      family = settings.font.numbers,
-      style = settings.font.style_map["Bold"],
-      size = 9.0,
-    },
-    color = colors.red,
-    string = "??? Bps",
-  },
-  y_offset = 4,
-})
-
-local wifi_down = sbar.add("item", "widgets.wifi2", {
-  position = "right",
-  padding_left = -5,
-  icon = {
-    padding_right = 0,
-    font = {
-      style = settings.font.style_map["Bold"],
-      size = 9.0,
-    },
-    string = icons.wifi.download,
-  },
-  label = {
-    font = {
-      family = settings.font.numbers,
-      style = settings.font.style_map["Bold"],
-      size = 9.0,
-    },
-    color = colors.blue,
-    string = "??? Bps",
-  },
-  y_offset = -4,
-})
-
 local wifi = sbar.add("item", "widgets.wifi.padding", {
   position = "right",
   label = { drawing = false },
@@ -63,115 +16,13 @@ local wifi = sbar.add("item", "widgets.wifi.padding", {
 -- Background around the item
 local wifi_bracket = sbar.add("bracket", "widgets.wifi.bracket", {
   wifi.name,
-  wifi_up.name,
-  wifi_down.name
 }, {
   background = { color = colors.bg1 },
   popup = { align = "center", height = 30 }
 })
 
-local ssid = sbar.add("item", {
-  position = "popup." .. wifi_bracket.name,
-  icon = {
-    font = {
-      style = settings.font.style_map["Bold"]
-    },
-    string = icons.wifi.router,
-  },
-  width = popup_width,
-  align = "center",
-  label = {
-    font = {
-      size = 15,
-      style = settings.font.style_map["Bold"]
-    },
-    max_chars = 18,
-    string = "????????????",
-  },
-  background = {
-    height = 2,
-    color = colors.grey,
-    y_offset = -15
-  }
-})
-
-local hostname = sbar.add("item", {
-  position = "popup." .. wifi_bracket.name,
-  icon = {
-    align = "left",
-    string = "Hostname:",
-    width = popup_width / 2,
-  },
-  label = {
-    max_chars = 20,
-    string = "????????????",
-    width = popup_width / 2,
-    align = "right",
-  }
-})
-
-local ip = sbar.add("item", {
-  position = "popup." .. wifi_bracket.name,
-  icon = {
-    align = "left",
-    string = "IP:",
-    width = popup_width / 2,
-  },
-  label = {
-    string = "???.???.???.???",
-    width = popup_width / 2,
-    align = "right",
-  }
-})
-
-local mask = sbar.add("item", {
-  position = "popup." .. wifi_bracket.name,
-  icon = {
-    align = "left",
-    string = "Subnet mask:",
-    width = popup_width / 2,
-  },
-  label = {
-    string = "???.???.???.???",
-    width = popup_width / 2,
-    align = "right",
-  }
-})
-
-local router = sbar.add("item", {
-  position = "popup." .. wifi_bracket.name,
-  icon = {
-    align = "left",
-    string = "Router:",
-    width = popup_width / 2,
-  },
-  label = {
-    string = "???.???.???.???",
-    width = popup_width / 2,
-    align = "right",
-  },
-})
-
 sbar.add("item", { position = "right", width = settings.group_paddings })
 
-wifi_up:subscribe("network_update", function(env)
-  local up_color = (env.upload == "000 Bps") and colors.grey or colors.red
-  local down_color = (env.download == "000 Bps") and colors.grey or colors.blue
-  wifi_up:set({
-    icon = { color = up_color },
-    label = {
-      string = env.upload,
-      color = up_color
-    }
-  })
-  wifi_down:set({
-    icon = { color = down_color },
-    label = {
-      string = env.download,
-      color = down_color
-    }
-  })
-end)
 
 wifi:subscribe({"wifi_change", "system_woke"}, function(env)
   sbar.exec("ipconfig getifaddr en0", function(ip)
@@ -189,6 +40,4 @@ local function toggle_details()
 	sbar.exec("osascript -e 'tell application \"System Events\" to tell process \"Control Center\" to perform action \"AXPress\" of menu bar item 3 of menu bar 1'")
 end
 
-wifi_up:subscribe("mouse.clicked", toggle_details)
-wifi_down:subscribe("mouse.clicked", toggle_details)
 wifi:subscribe("mouse.clicked", toggle_details)
