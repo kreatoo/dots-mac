e1be6a2e3ccb7cb856ece2478097bb3265a0794b
Kreato <kreato@kreato.dev>
2025-08-30 07:39:24 +0300

    hosts/akiri/apps: add anki, add xda to sdpi

--- hosts/akiri/apps.nix
+++ hosts/akiri/apps.nix
@@ -65,7 +65,7 @@
     user = {
 	  agents = {
 	    spoofdpi = {
-		  command = "${pkgs.spoofdpi}/bin/spoofdpi -enable-doh -window-size 1 -pattern '\\bdiscord\\b' -pattern '\\bdiscordapp\\b' -pattern '\\bnyaa\\b' -pattern '\\bgelbooru\\b' -pattern '\\bdanbooru\\b' -pattern '\\btumblr\\b' -pattern '\\b4chan\\b' -pattern '\\bhianime\\b' -pattern '\\bpastebin\\b' -pattern '\\bwikihow\\b' -pattern '\\bprotonvpn\\b' -pattern '\\be-hentai\\b' -pattern '\\bmullvad\\b' -pattern '\\bguilded\\b' -pattern '\\bsiker\\b'";
+		  command = "${pkgs.spoofdpi}/bin/spoofdpi -enable-doh -window-size 1 -pattern '\\bdiscord\\b' -pattern '\\bdiscordapp\\b' -pattern '\\bnyaa\\b' -pattern '\\bgelbooru\\b' -pattern '\\bdanbooru\\b' -pattern '\\btumblr\\b' -pattern '\\b4chan\\b' -pattern '\\bhianime\\b' -pattern '\\bpastebin\\b' -pattern '\\bwikihow\\b' -pattern '\\bprotonvpn\\b' -pattern '\\be-hentai\\b' -pattern '\\bmullvad\\b' -pattern '\\bguilded\\b' -pattern '\\bsiker\\b' -pattern '\\bxda-developers\\b'";
 		  serviceConfig = {
 		    RunAtLoad = true;
 		    KeepAlive = true;
@@ -101,6 +101,7 @@
 	  "sf-symbols"
       "imageoptim"
       "discord"
+      "anki"
 	  "font-sf-mono"
       "appcleaner"
       "omnidisksweeper"
