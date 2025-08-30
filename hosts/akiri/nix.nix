7758901776dbcb73a34cef289b292d6c00d47c67
Kreato <kreato@kreato.dev>
2025-08-30 07:38:58 +0300

    hosts/akiri/nix: enable gc and the like

--- hosts/akiri/nix.nix
+++ hosts/akiri/nix.nix
@@ -1,9 +1,25 @@
 { pkgs, ... }:
 
 {
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-  };
+  nix = {
+    settings = {
+        experimental-features = "nix-command flakes";
+    };
+    
+    gc = {
+      automatic = true;
+      interval = {
+          Weekday = 0;
+          Hour = 0;
+          Minute = 0;
+      };
+      options = "--delete-older-than 7d";
+    };
 
-  nix.package = pkgs.nix;
+    optimise = {
+        automatic = true;
+    };
+
+    package = pkgs.nix;
+  };
 }
